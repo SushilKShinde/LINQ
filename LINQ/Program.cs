c643d2a3e0e9893776eb2050047ceeba0e15edcc
Sushil <skshinde2028@gmail.com>
2023-06-13 22:03:11 +0530

    [Sushil] OrderBy with QuerySyntax and MethodSyntax

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -11,6 +11,18 @@ namespace LINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the linq concept");
+            List<int> dataSource = new List<int> { 100, 15, 74, 32, 21, 32, 14 };
+
+            var querySyntax = (from num in dataSource
+                               orderby num
+                               select num).ToList();
+
+            var methodSyntax = dataSource.OrderBy(num => num).ToList();
+
+            foreach (var item in methodSyntax)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
