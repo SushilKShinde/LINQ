2550e0d045e1b012bdc8bc79eed47ee7744a3a32
Sushil <skshinde2028@gmail.com>
2023-06-13 22:31:02 +0530

    [Sushil] OrderBy and Where on int and string list

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -11,17 +11,48 @@ namespace LINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the linq concept");
-            List<int> dataSource = new List<int> { 100, 15, 74, 32, 21, 32, 14 };
+            List<int> dataSource = new List<int> { 100, 15, 74, 32, 21, 35, 14 };
 
             var querySyntax = (from num in dataSource
+                               where num >= 30 && num <= 80
                                orderby num
                                select num).ToList();
 
-            var methodSyntax = dataSource.OrderBy(num => num).ToList();
+            var methodSyntax = dataSource.Where(num => num >= 30 && num <= 80).OrderBy(num => num).ToList();
 
+            Console.WriteLine("Output by Query Syntax");
+            foreach (var item in querySyntax)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nOutput by Method Syntax");
             foreach (var item in methodSyntax)
             {
                 Console.WriteLine(item);
+            }
+
+            List<string> stringDataSource = new List<string> { "Sushil", "Aman", "Budhha", "Chatur", "Deboline" };
+
+            var stringQuerySyntax = (from name in stringDataSource
+                                     where name.Length > 6
+                                     orderby name
+                                     select name).ToList();    // o/p: Deboline
+
+            var stringMethodSyntax = stringDataSource.Where(name => name.Length > 6).OrderBy(name => name).ToList(); // o/p: Deboline
+
+            Console.WriteLine("\nStringQuerySyntax OrderBy and Where");
+
+            foreach(var name in stringQuerySyntax)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("\nStringMethodSyntax OrderBy and where ");
+
+            foreach (var name in stringMethodSyntax)
+            {
+                Console.WriteLine(name);
             }
         }
     }
